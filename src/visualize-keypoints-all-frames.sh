3e81cea9d82dd3a56aae0b019019f00d804bc3ad
Jack Valmadre <jack.valmadre@gmail.com>
2012-07-11 09:58:27 -0400

    Added keypoint visualization.

---
+++ src/visualize-keypoints-all-frames.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 4 ]
+then
+  echo "usage: $0 image-format keypoints-format frame-format movie"
+  exit
+fi
+
+image_format=$1
+keypoints_format=$2
+frame_format=$3
+
+(( i = 1 ))
+while [ -e `printf $image_format $i` ]
+do
+  echo $i
+
+  image=`printf $image_format $i`
+  keypoints=`printf $keypoints_format $i`
+  frame=`printf $frame_format $i`
+
+  ./visualize-keypoints $image $keypoints --nodisplay --save \
+    --output_file=$frame
+
+  (( i += 1 ))
+done
+
+ffmpeg -y -i $image_format -sameq -vcodec mpeg4 $movie 2> /dev/null
---
+++ src/visualize_keypoints.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <algorithm>
+#include <cstdlib>
+#include <boost/bind.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <gflags/gflags.h>
+#include "read_image.hpp"
+#include "descriptor.hpp"
+#include "keypoint.hpp"
+#include "match.hpp"
+#include "random_color.hpp"
+
+const double SATURATION = 0.99;
+const double BRIGHTNESS = 0.99;
+
+DEFINE_string(output_file, "keypoints.png", "Location to save image.");
+DEFINE_bool(save, false, "Save to file?");
+DEFINE_bool(display, true, "Show in window?");
+
+typedef std::vector<cv::KeyPoint> KeypointList;
+
+// Renders a keypoint on top of an image with a random color.
+void drawKeypoint(cv::Mat& image, const cv::KeyPoint& keypoint) {
+  // Generate a random color.
+  cv::Scalar color = randomColor(SATURATION, BRIGHTNESS);
+
+  // Draw the keypoint in each image.
+  std::vector<cv::KeyPoint> single;
+  single.push_back(keypoint);
+  cv::drawKeypoints(image, single, image, color,
+      cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+}
+
+// Renders all keypoints over an image with random colors.
+void drawKeypoints(cv::Mat& image, const KeypointList& keypoints) {
+  // Draw each keypoint with a random color.
+  std::for_each(keypoints.begin(), keypoints.end(),
+      boost::bind(drawKeypoint, boost::ref(image), _1));
+}
+
+int main(int argc, char** argv) {
+  std::ostringstream usage;
+  usage << "Visualizes the keypoints detected in an image." << std::endl;
+  usage << std::endl;
+  usage << argv[0] << " image keypoints" << std::endl;
+
+  google::SetUsageMessage(usage.str());
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  if (argc != 3) {
+    google::ShowUsageWithFlags(argv[0]);
+    return 1;
+  }
+
+  std::string image_file = argv[1];
+  std::string keypoints_file = argv[2];
+  std::string output_file = FLAGS_output_file;
+
+  bool ok;
+
+  // Load images.
+  cv::Mat image;
+  cv::Mat gray_image;
+  ok = readImage(image_file, image, gray_image);
+  if (!ok) {
+    std::cerr << "could not load image" << std::endl;
+    return 1;
+  }
+
+  // Load keypoints.
+  KeypointList keypoints;
+  ok = loadKeypoints(keypoints_file, keypoints);
+  if (!ok) {
+    std::cerr << "could not load keypoints" << std::endl;
+    return 1;
+  }
+
+  // Visualize matches.
+  drawKeypoints(image, keypoints);
+
+  if (FLAGS_save) {
+    cv::imwrite(output_file, image);
+  }
+
+  if (FLAGS_display) {
+    cv::imshow("keypoints", image);
+    cv::waitKey(0);
+  }
+
+  return 0;
+}
