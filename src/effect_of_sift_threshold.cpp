1d5bf10d514234f0df4d687065d04f81e2f66468
Jack Valmadre <jack.valmadre@gmail.com>
2012-08-03 15:41:43 -0400

    Fleshed out effect-of-sift-threshold.

--- src/effect_of_sift_threshold.cpp
+++ src/effect_of_sift_threshold.cpp
@@ -4,8 +4,8 @@
 #include <list>
 #include <iterator>
 #include <algorithm>
-#include <boost/bind.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/format.hpp>
 #include <opencv2/core/core.hpp>
 #include <gflags/gflags.h>
 #include <glog/logging.h>
@@ -19,6 +19,10 @@ typedef std::vector<Descriptor> DescriptorList;
 
 // List of matches with detailed results.
 typedef std::vector<cv::DMatch> MatchResultList;
+
+std::string makeFilename(const std::string& format, double threshold, int n) {
+  return boost::str(boost::format(format) % threshold % (n + 1));
+}
 
 void listToMatrix(const DescriptorList& list, cv::Mat& matrix) {
   int rows = list.size();
@@ -80,88 +84,38 @@ struct IsInconsistent {
   }
 };
 
-int main(int argc, char** argv) {
-  std::ostringstream usage;
-  usage << "Evaluates effect of SIFT threshold on matches and speed." <<
-    std::endl;
-  usage << std::endl;
-  usage << "Sample usage:" << std::endl;
-  usage << argv[0] << " descriptors1 descriptors2" << std::endl;
-  usage << std::endl;
-  usage << "Parameters:" << std::endl;
-  usage << "descriptors1, descriptors2 -- Input. Descriptors to match." <<
-    std::endl;
-
-  google::SetUsageMessage(usage.str());
-  google::ParseCommandLineFlags(&argc, &argv, true);
-  google::InitGoogleLogging(argv[0]);
-
-  if (argc != 3) {
-    google::ShowUsageWithFlags(argv[0]);
-    return 1;
-  }
-
-  std::string descriptors_file1 = argv[1];
-  std::string descriptors_file2 = argv[2];
-
-  bool ok;
-
-  // Load descriptors.
-  DescriptorList descriptors1;
-  DescriptorList descriptors2;
-
-  ok = loadDescriptors(descriptors_file1, descriptors1);
-  CHECK(ok) << "Could not load descriptors!";
-
-  ok = loadDescriptors(descriptors_file2, descriptors2);
-  CHECK(ok) << "Could not load descriptors!";
-
+void match(const DescriptorList& descriptors1,
+           const DescriptorList& descriptors2,
+           MatchList& matches,
+           bool use_flann,
+           double& time) {
   // Load descriptors into matrices for cv::DescriptorMatcher.
   cv::Mat mat1;
   cv::Mat mat2;
   listToMatrix(descriptors1, mat1);
   listToMatrix(descriptors2, mat2);
 
-  // Create matcher.
-  cv::Ptr<cv::DescriptorMatcher> matcher;
-  matcher = cv::DescriptorMatcher::create("FlannBased");
-
   // Find two nearest neighbours, match in each direction.
   std::vector<MatchResultList> forward_match_lists;
   std::vector<MatchResultList> reverse_match_lists;
 
-  std::cerr << "Forward matching..." << std::endl;
+  clock_t t = clock();
+
+  // Construct matcher.
+  cv::Ptr<cv::DescriptorMatcher> matcher;
+  if (use_flann) {
+    matcher = cv::DescriptorMatcher::create("FlannBased");
+  } else {
+    matcher = cv::DescriptorMatcher::create("BruteForce");
+  }
+
+  // Match all points in both directions.
+  // Get top two matches as this is what we'll be doing in practice.
   matcher->knnMatch(mat1, mat2, forward_match_lists, 2);
-  std::cerr << forward_match_lists.size() << " matches found" << std::endl;
-
-  std::cerr << "Reverse matching..." << std::endl;
   matcher->knnMatch(mat2, mat1, reverse_match_lists, 2);
-  std::cerr << reverse_match_lists.size() << " matches found" << std::endl;
 
-  /*
-  // Prune under a certain threshold.
-  std::cerr << "Enforcing distinctive forward matches..." << std::endl;
-  {
-    std::vector<MatchResultList> distinctive;
-    std::remove_copy_if(forward_match_lists.begin(), forward_match_lists.end(),
-        std::back_inserter(distinctive),
-        boost::bind(isNotDistinctive, _1, min_relative_distance));
-    forward_match_lists.swap(distinctive);
-  }
-  std::cerr << forward_match_lists.size() << " forward matches remain" <<
-    std::endl;
-
-  std::cerr << "Enforcing distinctive reverse matches..." << std::endl;
-  {
-    std::vector<MatchResultList> distinctive;
-    std::remove_copy_if(reverse_match_lists.begin(), reverse_match_lists.end(),
-        std::back_inserter(distinctive),
-        boost::bind(isNotDistinctive, _1, min_relative_distance));
-    reverse_match_lists.swap(distinctive);
-  }
-  std::cerr << reverse_match_lists.size() << " reverse matches remain" <<
-    std::endl;
-  */
+  t = clock() - t;
+  time = double(t) / CLOCKS_PER_SEC;
 
   // Keep only first match.
   MatchList forward_matches;
@@ -172,11 +126,103 @@ int main(int argc, char** argv) {
       std::back_inserter(reverse_matches), extractFirstMatch);
 
   // Now check consistency in both directions.
-  std::cerr << "Enforcing consistency..." << std::endl;
-  MatchList matches;
   std::remove_copy_if(forward_matches.begin(), forward_matches.end(),
       std::back_inserter(matches), IsInconsistent(reverse_matches));
-  std::cerr << matches.size() << " matches remain" << std::endl;
+
+  LOG(INFO) << forward_matches.size() << " forward matches";
+  LOG(INFO) << reverse_matches.size() << " reverse matches";
+  LOG(INFO) << matches.size() << " consistent matches";
+}
+
+int main(int argc, char** argv) {
+  std::ostringstream usage;
+  usage << "Evaluates effect of SIFT threshold on matches and speed." <<
+    std::endl;
+  usage << std::endl;
+  usage << "Sample usage:" << std::endl;
+  usage << argv[0] << " descriptors1-format descriptors2-format base-threshold "
+    "threshold-step num-thresholds" << std::endl;
+
+  google::SetUsageMessage(usage.str());
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+
+  if (argc != 3) {
+    google::ShowUsageWithFlags(argv[0]);
+    return 1;
+  }
+
+  std::string descriptors_format1 = argv[1];
+  std::string descriptors_format2 = argv[2];
+  double base_threshold = boost::lexical_cast<double>(argv[3]);
+  double threshold_step = boost::lexical_cast<double>(argv[4]);
+  int num_thresholds = boost::lexical_cast<int>(argv[5]);
+
+  bool ok = true;
+
+  for (int i = 0; i < num_thresholds && ok; i += 1) {
+    double threshold = base_threshold * std::pow(threshold_step, i);
+
+    LOG(INFO) << "Threshold: " << threshold << std::endl;
+
+    double mean_num_descriptors = 0;
+    double mean_num_exhaustive_matches = 0;
+    double mean_exhaustive_time = 0;
+    double mean_num_flann_matches = 0;
+    double mean_flann_time = 0;
+
+    int t = 0;
+    while (ok) {
+      // Build filenames.
+      std::string descriptors_file1 = makeFilename(descriptors_format1,
+          threshold, t);
+      std::string descriptors_file2 = makeFilename(descriptors_format2,
+          threshold, t);
+
+      // Load descriptors.
+      DescriptorList descriptors1;
+      DescriptorList descriptors2;
+
+      ok = loadDescriptors(descriptors_file1, descriptors1);
+      if (!ok) {
+        continue;
+      }
+      ok = loadDescriptors(descriptors_file2, descriptors2);
+      if (!ok) {
+        continue;
+      }
+
+      double exhaustive_time;
+      double flann_time;
+      MatchList exhaustive_matches;
+      MatchList flann_matches;
+
+      // Match the two lists of descriptors using brute force and FLANN.
+      match(descriptors1, descriptors2, exhaustive_matches, false,
+          exhaustive_time);
+      match(descriptors1, descriptors2, flann_matches, false, flann_time);
+
+      mean_num_descriptors += descriptors1.size();
+      mean_num_descriptors += descriptors2.size();
+      mean_num_exhaustive_matches += exhaustive_matches.size();
+      mean_exhaustive_time += exhaustive_time;
+      mean_num_flann_matches += flann_matches.size();
+      mean_flann_time += flann_time;
+    }
+
+    mean_num_descriptors /= 2 * t;
+    mean_num_exhaustive_matches /= t;
+    mean_exhaustive_time /= t;
+    mean_num_flann_matches /= t;
+    mean_flann_time /= t;
+
+    std::cout << threshold << "\t";
+    std::cout << mean_num_descriptors << "\t";
+    std::cout << mean_num_exhaustive_matches << "\t";
+    std::cout << mean_exhaustive_time << "\t";
+    std::cout << mean_num_flann_matches << "\t";
+    std::cout << mean_flann_time << std::endl;
+  }
 
   return 0;
 }
