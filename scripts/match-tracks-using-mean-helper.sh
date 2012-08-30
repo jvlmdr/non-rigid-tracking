2c0fef13ae8caf76122d73652618daf071f94ff5
Jack Valmadre <jack.valmadre@gmail.com>
2012-08-29 20:55:05 -0400

    Fixed bug in mean track matching.

--- scripts/match-tracks-using-mean-helper.sh
+++ scripts/match-tracks-using-mean-helper.sh
@@ -13,5 +13,9 @@ time1=$4
 view2=$5
 time2=$6
 
+descriptors1=`printf $descriptors_format $view1 $time1`
+descriptors2=`printf $descriptors_format $view2 $time2`
+matches=`printf $matches_format $view1 $view2 $time1 $time2`
+
 echo "($view1, $time1), ($view2, $time2)"
 ./match-tracks-using-mean $descriptors1 $descriptors2 $matches -logtostderr
