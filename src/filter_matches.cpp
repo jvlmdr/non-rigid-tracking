e804235b3db9157956ee0eada4eb3fa9b9333658
Jack Valmadre <jack.valmadre@gmail.com>
2012-10-29 10:50:17 -0400

    Added filtering program.

--- src/filter_matches.cpp
+++ src/filter_matches.cpp
@@ -1,33 +1,23 @@
 #include <string>
 #include <vector>
-#include <list>
-#include <iterator>
 #include <algorithm>
 #include <boost/bind.hpp>
 #include <boost/lexical_cast.hpp>
-#include <opencv2/core/core.hpp>
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 
-#include "read_image.hpp"
-#include "descriptor.hpp"
 #include "match_result.hpp"
 #include "unique_match_result.hpp"
-#include "find_matches.hpp"
-#include "find_unique_matches.hpp"
 
-#include "descriptor_reader.hpp"
+#include "iterator_reader.hpp"
 #include "match_result_reader.hpp"
 #include "unique_match_result_reader.hpp"
-#include "iterator_reader.hpp"
 
 #include "iterator_writer.hpp"
-#include "match_writer.hpp"
+#include "match_result_writer.hpp"
 
 DEFINE_bool(unique, true,
     "Each feature is associated to at most one other feature");
-
-DEFINE_bool(reciprocal, false, "Require matches to be reciprocal");
 
 DEFINE_bool(use_clearance, false,
     "Require the best match to be sufficiently far from the second-best");
@@ -44,6 +34,8 @@ DEFINE_double(absolute_threshold, 1, "The absolute distance threshold");
 //    "Keep matches that are within a fraction of the distance of the best "
 //    "match");
 //DEFINE_double(relative_threshold, 0.7, "The relative distance threshold");
+
+////////////////////////////////////////////////////////////////////////////////
 
 bool matchIsUndistinctive(const UniqueMatchResult& match, double ratio) {
   // Pick nearest second-best match.
@@ -113,18 +105,6 @@ void removePoorMatches(std::vector<MatchResult>& matches,
 
 ////////////////////////////////////////////////////////////////////////////////
 
-MatchResult flipResult(const MatchResult& result) {
-  return MatchResult(result.index2, result.index1, result.distance);
-}
-
-UniqueMatchResult flipUniqueResult(const UniqueMatchResult& result) {
-  return result.flip();
-}
-
-Match resultToMatch(const MatchResult& result) {
-  return Match(result.index1, result.index2);
-}
-
 MatchResult uniqueResultToResult(const UniqueMatchResult& result) {
   return MatchResult(result.index1, result.index2, result.distance);
 }
@@ -135,13 +115,13 @@ void init(int& argc, char**& argv) {
   std::ostringstream usage;
   usage << "Computes matches between sets of descriptors." << std::endl;
   usage << std::endl;
-  usage << argv[0] << " forward-matches reverse-matches matches" << std::endl;
+  usage << argv[0] << " input-matches output-matches" << std::endl;
   google::SetUsageMessage(usage.str());
 
   google::InitGoogleLogging(argv[0]);
   google::ParseCommandLineFlags(&argc, &argv, true);
 
-  if (argc != 4) {
+  if (argc != 3) {
     google::ShowUsageWithFlags(argv[0]);
     std::exit(1);
   }
@@ -150,85 +130,44 @@ void init(int& argc, char**& argv) {
 int main(int argc, char** argv) {
   init(argc, argv);
 
-  std::string forward_file = argv[1];
-  std::string reverse_file = argv[2];
-  std::string matches_file = argv[3];
+  std::string input_file = argv[1];
+  std::string output_file = argv[2];
 
   bool ok;
 
-  std::vector<MatchResult> forward_results;
-  std::vector<MatchResult> reverse_results;
+  std::vector<MatchResult> matches;
 
   if (FLAGS_unique) {
     // If unique matches were found, can threshold clearance.
-    std::vector<UniqueMatchResult> unique_forward;
-    std::vector<UniqueMatchResult> unique_reverse;
+    std::vector<UniqueMatchResult> unique_matches;
 
     UniqueMatchResultReader match_reader;
-    ok = loadList(forward_file, unique_forward, match_reader);
-    CHECK(ok) << "Could not load forward matches";
-    LOG(INFO) << "Loaded " << unique_forward.size() << " forward matches";
-    ok = loadList(reverse_file, unique_reverse, match_reader);
-    CHECK(ok) << "Could not load reverse matches";
-    LOG(INFO) << "Loaded " << unique_reverse.size() << " reverse matches";
-
-    // Flip order of reverse matches.
-    std::transform(unique_reverse.begin(), unique_reverse.end(),
-        unique_reverse.begin(), flipUniqueResult);
+    ok = loadList(input_file, unique_matches, match_reader);
+    CHECK(ok) << "Could not load matches";
+    LOG(INFO) << "Loaded " << unique_matches.size() << " matches";
 
     if (FLAGS_use_clearance) {
       // Filter results that are not distinctive.
-      removeUndistinctiveMatches(unique_forward, true, FLAGS_clearance);
-      removeUndistinctiveMatches(unique_reverse, false, FLAGS_clearance);
+      removeUndistinctiveMatches(unique_matches, true, FLAGS_clearance);
     }
 
-    // Remove extra information.
-    std::transform(unique_forward.begin(), unique_forward.end(),
-        std::back_inserter(forward_results), uniqueResultToResult);
-    std::transform(unique_reverse.begin(), unique_reverse.end(),
-        std::back_inserter(reverse_results), uniqueResultToResult);
+    // Convert to plain old matches (without next-best information).
+    std::transform(unique_matches.begin(), unique_matches.end(),
+        std::back_inserter(matches), uniqueResultToResult);
   } else {
     MatchResultReader match_reader;
-    ok = loadList(forward_file, forward_results, match_reader);
+    ok = loadList(input_file, matches, match_reader);
     CHECK(ok) << "Could not load forward matches";
-    LOG(INFO) << "Loaded " << forward_results.size() << " forward matches";
-    ok = loadList(reverse_file, reverse_results, match_reader);
-    CHECK(ok) << "Could not load reverse matches";
-    LOG(INFO) << "Loaded " << reverse_results.size() << " reverse matches";
-
-    // Flip order of reverse matches.
-    std::transform(reverse_results.begin(), reverse_results.end(),
-        reverse_results.begin(), flipResult);
+    LOG(INFO) << "Loaded " << matches.size() << " forward matches";
   }
 
   if (FLAGS_use_absolute_threshold) {
     // Filter matches that are below a threshold.
-    removePoorMatches(forward_results, FLAGS_absolute_threshold, true);
-    removePoorMatches(reverse_results, FLAGS_absolute_threshold, false);
+    removePoorMatches(matches, FLAGS_absolute_threshold, true);
   }
 
-  std::vector<Match> forward_matches;
-  std::vector<Match> reverse_matches;
-
-  // Remove extra information.
-  std::transform(forward_results.begin(), forward_results.end(),
-      std::back_inserter(forward_matches), resultToMatch);
-  std::transform(reverse_results.begin(), reverse_results.end(),
-      std::back_inserter(reverse_matches), resultToMatch);
-
-  std::vector<Match> matches;
-  if (FLAGS_reciprocal) {
-    // Reduce to a consistent set.
-    intersectionOfMatches(forward_matches, reverse_matches, matches);
-    LOG(INFO) << "Found " << matches.size() << " reciprocal matches";
-  } else {
-    // Throw all matches together.
-    unionOfMatches(forward_matches, reverse_matches, matches);
-    LOG(INFO) << "Found " << matches.size() << " matches";
-  }
-
-  MatchWriter match_writer;
-  ok = saveList(matches_file, matches, match_writer);
+  MatchResultWriter match_writer;
+  ok = saveList(output_file, matches, match_writer);
   CHECK(ok) << "Could not save list of matches";
 
   return 0;
