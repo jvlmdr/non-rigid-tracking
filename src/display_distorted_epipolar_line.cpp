b3bf6f48f793ed8ffcfcfb92c012cd3222b2170c
Jack Valmadre <jack.valmadre@gmail.com>
2012-10-31 16:17:08 -0400

    Cleaned up distorted epipolar line code

--- src/display_distorted_epipolar_line.cpp
+++ src/display_distorted_epipolar_line.cpp
@@ -44,7 +44,7 @@ std::vector<cv::Point2d> lineCircleIntersection(double r,
                                                 double e1,
                                                 double e2,
                                                 double e3) {
-  std::vector<cv::Point2d> solutions;
+  std::vector<cv::Point2d> roots;
 
   // x2 = -(e1 x1 + e3) / e2
   // x1^2 + x2^2 = r^2
@@ -59,7 +59,7 @@ std::vector<cv::Point2d> lineCircleIntersection(double r,
   if (delta == 0) {
     double u = -b / (2. * a);
     double v = -(e1 * u + e3) / e2;
-    solutions.push_back(cv::Point2d(u, v));
+    roots.push_back(cv::Point2d(u, v));
   } else if (delta >= 0) {
     double u;
     double v;
@@ -67,14 +67,14 @@ std::vector<cv::Point2d> lineCircleIntersection(double r,
     u = (-b + std::sqrt(delta)) / (2. * a);
     // TODO: This may be unstable if e2 is close to zero?
     v = -(e1 * u + e3) / e2;
-    solutions.push_back(cv::Point2d(u, v));
+    roots.push_back(cv::Point2d(u, v));
 
     u = (-b - std::sqrt(delta)) / (2. * a);
     v = -(e1 * u + e3) / e2;
-    solutions.push_back(cv::Point2d(u, v));
+    roots.push_back(cv::Point2d(u, v));
   }
 
-  return solutions;
+  return roots;
 }
 
 bool fourConnected(const cv::Point2d& p, const cv::Point2d& q) {
@@ -91,6 +91,140 @@ bool fourConnected(const cv::Point2d& p, const cv::Point2d& q) {
   }
 
   return connected;
+}
+
+bool operator<(const cv::Point& p, const cv::Point& q) {
+  if (p.x != p.y) {
+    return p.x < q.x;
+  } else {
+    return p.y < q.y;
+  }
+}
+
+struct ComparePoints {
+  bool operator()(const cv::Point& p, const cv::Point& q) {
+    return p < q;
+  }
+};
+
+typedef std::set<cv::Point, ComparePoints> PixelSet;
+
+class DistortedEpipolarLineFinder {
+  public:
+    DistortedEpipolarLineFinder(const CameraProperties& camera2,
+                                const cv::Matx33d& F);
+
+    void init();
+
+    // Computes an epipolar line given 
+    void compute(const cv::Point2d& x, PixelSet& line) const;
+
+  private:
+    const CameraProperties* camera_;
+    cv::Mat F_;
+    double radius_;
+};
+
+DistortedEpipolarLineFinder::DistortedEpipolarLineFinder(
+    const CameraProperties& camera,
+    const cv::Matx33d& F) : camera_(&camera), F_(F) {}
+
+void DistortedEpipolarLineFinder::init() {
+  // Get the four points at the bounds of image 2.
+  std::vector<cv::Point2d> corners;
+  int w = camera_->image_size.width;
+  int h = camera_->image_size.width;
+  corners.push_back(cv::Point2d(    0,     0));
+  corners.push_back(cv::Point2d(w - 1,     0));
+  corners.push_back(cv::Point2d(    0, h - 1));
+  corners.push_back(cv::Point2d(w - 1, h - 1));
+
+  // Convert to homogeneous co-ordinates.
+  cv::Mat X;
+  imagePointsToHomogeneous(corners, X);
+  // Undo intrinsics.
+  cv::Mat K2(camera_->matrix());
+  X = K2.inv() * X;
+  // Convert back to inhomogeneous co-ordinates.
+  imagePointsFromHomogeneous(X, corners);
+
+  // Find radius of each distorted point.
+  std::vector<double> lengths;
+  double (*take_norm)(const cv::Point2d&) = cv::norm;
+  std::transform(corners.begin(), corners.end(), std::back_inserter(lengths),
+      take_norm);
+  // Find maximum radius.
+  double distorted_radius = std::accumulate(lengths.begin(), lengths.end(), 0.,
+      std::max<double>);
+
+  // Check if corner radius exceeds maximum radius.
+  double max_distorted_radius = 0.99 * maxDistortedRadius(camera_->distort_w);
+  if (distorted_radius > max_distorted_radius) {
+    LOG(WARNING) << "Clipping radius from " << distorted_radius << " to " <<
+        max_distorted_radius;
+    distorted_radius = max_distorted_radius;
+  }
+
+  radius_ = undistortRadius(distorted_radius, camera_->distort_w);
+}
+
+// x1 must be undistorted.
+void DistortedEpipolarLineFinder::compute(const cv::Point2d& x1,
+                                          PixelSet& line) const {
+  typedef std::pair<double, double> Interval;
+
+  line.clear();
+
+  // Find co-ordinates of epipolar line. Given an uncalibrated point in image 1,
+  // we want an equation for the calibrated point in image 2.
+  cv::Mat K2(camera_->matrix());
+  cv::Mat X1 = imagePointToHomogeneous(x1);
+  cv::Mat e = K2.t() * F_ * X1;
+  // May as well normalize for numeric nicety.
+  e = e / cv::norm(e);
+
+  // Find intersections.
+  std::vector<cv::Point2d> roots = lineCircleIntersection(radius_,
+      e.at<double>(0, 0), e.at<double>(1, 0), e.at<double>(2, 0));
+
+  if (roots.size() == 2) {
+    const cv::Point2d& a = roots[0];
+    const cv::Point2d& b = roots[1];
+
+    std::stack<Interval> intervals;
+    intervals.push(Interval(0, 1));
+
+    while (!intervals.empty()) {
+      // Pull next interval (s, t) off the stack.
+      double s = intervals.top().first;
+      double t = intervals.top().second;
+      intervals.pop();
+
+      // Find interval endpoints in 2D.
+      cv::Point2d u = (1 - s) * a + s * b;
+      cv::Point2d v = (1 - t) * a + t * b;
+      // Apply distortion.
+      u = distort(u, camera_->distort_w);
+      v = distort(v, camera_->distort_w);
+      // Apply intrinsics.
+      u = imagePointFromHomogeneous(K2 * imagePointToHomogeneous(u));
+      v = imagePointFromHomogeneous(K2 * imagePointToHomogeneous(v));
+
+      // Quantize to pixels.
+      cv::Point p = u;
+      cv::Point q = v;
+      // Add to line.
+      line.insert(p);
+      line.insert(q);
+
+      // Recurse if points are not connected.
+      if (!fourConnected(p, q)) {
+        double mu = (s + t) / 2.;
+        intervals.push(Interval(mu, t));
+        intervals.push(Interval(s, mu));
+      }
+    }
+  }
 }
 
 int main(int argc, char** argv) {
@@ -124,46 +258,10 @@ int main(int argc, char** argv) {
   ok = loadList(keypoints_file, keypoints, keypoint_reader);
   CHECK(ok) << "Could not load keypoints";
 
-  // Get intrinics matrix for first camera.
+  DistortedEpipolarLineFinder line_finder(camera2, F);
+  line_finder.init();
+
   cv::Mat K1(camera1.matrix());
-  cv::Mat K2(camera2.matrix());
-
-  // Get the four points at the bounds of the second image.
-  std::vector<cv::Point2d> corners;
-  int w = camera2.image_size.width;
-  int h = camera2.image_size.width;
-  corners.push_back(cv::Point2d(    0,     0));
-  corners.push_back(cv::Point2d(w - 1,     0));
-  corners.push_back(cv::Point2d(    0, h - 1));
-  corners.push_back(cv::Point2d(w - 1, h - 1));
-
-  // Convert to homogeneous co-ordinates.
-  cv::Mat X;
-  imagePointsToHomogeneous(corners, X);
-  // Undo intrinsics.
-  X = K2.inv() * X;
-  // Convert back to inhomogeneous co-ordinates.
-  imagePointsFromHomogeneous(X, corners);
-
-  // Find radius of each point.
-  std::vector<double> radii;
-  double (*take_norm)(const cv::Point2d&) = cv::norm;
-  std::transform(corners.begin(), corners.end(), std::back_inserter(radii),
-      take_norm);
-
-  // Find maximum radius.
-  double distorted_radius = std::accumulate(radii.begin(), radii.end(), 0.,
-      std::max<double>);
-
-  double max_distorted_radius = 0.99 * maxDistortedRadius(camera2.distort_w);
-  if (distorted_radius > max_distorted_radius) {
-    LOG(INFO) << "Clipping radius from " << distorted_radius << " to " <<
-        max_distorted_radius;
-    distorted_radius = max_distorted_radius;
-  }
-
-  double undistorted_radius = undistortRadius(distorted_radius,
-      camera2.distort_w);
 
   std::vector<SiftPosition>::const_iterator keypoint;
   for (keypoint = keypoints.begin(); keypoint != keypoints.end(); ++keypoint) {
@@ -173,76 +271,24 @@ int main(int argc, char** argv) {
     x1 = undistort(x1, camera1.distort_w);
     x1 = imagePointFromHomogeneous(K1 * imagePointToHomogeneous(x1));
 
-    cv::Mat X1 = imagePointToHomogeneous(x1);
+    // Find line.
+    PixelSet line;
+    line_finder.compute(x1, line);
 
-    // Find co-ordinates of epipolar line. Given an uncalibrated point in image
-    // 1, we want an equation for the calibrated point in image 2.
-    cv::Mat e = K2.t() * F * X1;
-    // May as well normalize.
-    e = e / cv::norm(e);
+    // Draw line.
+    cv::Mat image = cv::Mat_<uint8_t>(camera2.image_size, 0);
+    PixelSet::const_iterator pixel;
 
-    // Find intersections.
-    std::vector<cv::Point2d> solutions = lineCircleIntersection(
-        undistorted_radius, e.at<double>(0, 0), e.at<double>(1, 0),
-        e.at<double>(2, 0));
+    cv::Rect bounds(cv::Point(0, 0), camera2.image_size);
 
-    if (solutions.size() == 2) {
-      cv::Point2d a = solutions[0];
-      cv::Point2d b = solutions[1];
-
-      std::set<std::pair<int, int> > line;
-      std::stack<std::pair<double, double> > intervals;
-      intervals.push(std::pair<double, double>(0, 1));
-
-      while (!intervals.empty()) {
-        // Pull next element off the stack.
-        double s = intervals.top().first;
-        double t = intervals.top().second;
-        intervals.pop();
-
-        cv::Point2d u = (1 - s) * a + s * b;
-        cv::Point2d v = (1 - t) * a + t * b;
-
-        // Apply distortion to points.
-        u = distort(u, camera2.distort_w);
-        v = distort(v, camera2.distort_w);
-
-        // Apply intrinsics to points.
-        u = imagePointFromHomogeneous(K2 * imagePointToHomogeneous(u));
-        v = imagePointFromHomogeneous(K2 * imagePointToHomogeneous(v));
-
-        // Quantize to pixels.
-        cv::Point p = u;
-        cv::Point q = v;
-
-        // Add to line.
-        line.insert(std::pair<int, int>(p.x, p.y));
-        line.insert(std::pair<int, int>(q.x, q.y));
-
-        // Check if they are connected.
-        if (!fourConnected(p, q)) {
-          double mu = (s + t) / 2.;
-          intervals.push(std::pair<double, double>(mu, t));
-          intervals.push(std::pair<double, double>(s, mu));
-        }
+    for (pixel = line.begin(); pixel != line.end(); ++pixel) {
+      if (bounds.contains(*pixel)) {
+        image.at<uint8_t>(pixel->y, pixel->x) = 255;
       }
-
-      // Draw line.
-      cv::Mat image = cv::Mat_<uint8_t>(camera2.image_size, 0);
-      std::set<std::pair<int, int> >::const_iterator pixel;
-
-      cv::Rect bounds(cv::Point(0, 0), camera2.image_size);
-
-      for (pixel = line.begin(); pixel != line.end(); ++pixel) {
-        cv::Point position(pixel->first, pixel->second);
-        if (bounds.contains(position)) {
-          image.at<uint8_t>(pixel->second, pixel->first) = 255;
-        }
-      }
-
-      cv::imshow("line", image);
-      cv::waitKey(10);
     }
+
+    cv::imshow("line", image);
+    cv::waitKey(10);
   }
 
   return 0;
