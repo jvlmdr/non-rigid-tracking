b9104ae4d0319ac8b87873513dfc58a0faca4ba9
Jack Valmadre <jack.valmadre@gmail.com>
2012-12-13 21:31:18 -0500

    Set up framework for multi-view DP

--- src/find_multiview_track.cpp
+++ src/find_multiview_track.cpp
@@ -14,6 +14,8 @@
 #include "camera.hpp"
 #include "distortion.hpp"
 #include "util.hpp"
+#include "quantize_ray.hpp"
+#include "viterbi.hpp"
 
 #include "track_list_reader.hpp"
 #include "image_point_reader.hpp"
@@ -23,10 +25,7 @@
 #include "multiview_track_list_writer.hpp"
 #include "image_point_writer.hpp"
 
-struct OtherView {
-  int index;
-  Camera camera;
-};
+DEFINE_double(delta, 1, "Minimum resolution (in pixels) per quantization");
 
 std::pair<int, cv::Point2d> calibrateIndexedPoint(
     const std::pair<int, cv::Point2d>& point,
@@ -78,188 +77,120 @@ Track<cv::Point2d> calibrateAndUndistortTrack(
 
 ////////////////////////////////////////////////////////////////////////////////
 
-Camera extractCameraFromOtherView(const OtherView& view) {
-  return view.camera;
-}
-
-double errorInDistanceFromPoint(cv::Point2d y,
-                                double lambda,
-                                const cv::Mat A,
-                                const cv::Mat B,
-                                double delta,
-                                const CameraProperties& intrinsics) {
-  cv::Mat X = A + lambda * B;
-  cv::Point2d x = imagePointFromHomogeneous(X);
-  x = intrinsics.distortAndUncalibrate(x);
-  return cv::norm(x - y) - delta;
-}
-
-// projection -- Already calibrated and undistorted.
-void findExtentOfRay(const cv::Point2d& projection,
-                     const CameraPose& camera,
-                     const std::vector<Camera>& others) {
-  cv::Point2d w = projection;
-
-  // Camera center.
-  cv::Point3d c(camera.center);
-
-  // Find vector in nullspace of linear projection system, A = R_xy - w R_z.
-  cv::Mat R(camera.rotation);
-  cv::Mat A = R.rowRange(0, 2) - cv::Mat(w) * R.rowRange(2, 3);
-  // 1D nullspace found trivially by cross-product.
-  // Take negative i x j because z < 0 is in front of camera.
-  cv::Mat V = -A.row(0).t().cross(A.row(1).t());
-  cv::Point3d v(V.at<double>(0, 0), V.at<double>(0, 1), V.at<double>(0, 2));
-
-  // Space of solutions parametrized by 3D line c + lambda v, with lambda >= 0.
-
-  // For each image, find intersections of the projected line with the outer
-  // radius of the lens distortion.
-  std::vector<Camera>::const_iterator other;
-  for (other = others.begin(); other != others.end(); ++other) {
-    cv::Mat P(other->extrinsics().matrix());
-
-    cv::Mat C = worldPointToHomogeneous(c);
-    cv::Mat V = worldPointToHomogeneous(v, 0);
-    cv::Mat A = P * C;
-    cv::Mat B = P * V;
-
-    // Assume that line has a vanishing point (b is not at infinity).
-    // TODO: Cope with non-vanishing-point case (b at infinity).
-    //cv::Point2d a = imagePointFromHomogeneous(A);
-    //CHECK(B.at<double>(2, 0) != 0);
-    //cv::Point2d b = imagePointFromHomogeneous(B);
-
-    double a3 = A.at<double>(2, 0);
-    double b3 = B.at<double>(2, 0);
-
-    if (a3 > 0 && b3 > 0) {
-      // Entire ray is behind camera.
-      DLOG(INFO) << "Ray is not observed";
-      continue;
-    }
-
-    double delta = 1;
-
-    double lambda;
-    cv::Point2d x;
-    double lambda_min;
-
-    if (a3 < 0) {
-      // Ray starts in front of camera. Line starts at a finite coordinate.
-      DLOG(INFO) << "Ray starts in front of camera";
-      lambda_min = 0;
-    } else {
-      // Ray starts behind camera. Line starts at infinity.
-      DLOG(INFO) << "Ray starts behind camera";
-      lambda_min = -a3 / b3;
-    }
-
-    if (b3 < 0) {
-      // Ray goes to infinity in front of camera. There is a vanishing point.
-      DLOG(INFO) << "Ray ends in front of camera";
-      CHECK(B.at<double>(2, 0) != 0);
-      x = imagePointFromHomogeneous(B);
-      x = other->intrinsics().distortAndUncalibrate(x);
-
-      // We can't use lambda = infinity for bisection.
-      // Find a lambda which is big enough.
-      lambda = 1.;
-      bool found = false;
-      while (!found) {
-        double error = errorInDistanceFromPoint(x, lambda, A, B, delta,
-            other->intrinsics());
-        if (error < 0) {
-          found = true;
-        }
-        lambda *= 2;
-      }
-    } else {
-      // Ray goes to infinity behind camera, crossing image plane.
-      // There is no vanishing point. However, under distortion, a 2D point at
-      // infinity will still have a finite position.
-      DLOG(INFO) << "Ray ends behind camera";
-      lambda = -a3 / b3;
-      cv::Mat X = A + lambda * B;
-      x = cv::Point2d(X.at<double>(0, 0), X.at<double>(1, 0));
-      x = distortPointAtInfinity(x, other->intrinsics().distort_w);
-      x = other->intrinsics().uncalibrate(x);
-    }
-
-    std::vector<double> lambdas;
-    bool converged = false;
-
-    while (!converged) {
-      // Check there is a point on the line at least delta pixels away from x.
-      double f_max = errorInDistanceFromPoint(x, lambda_min, A, B, delta,
-            other->intrinsics());
-
-      if (f_max < 0) {
-        // No solution is possible.
-        converged = true;
-      } else {
-        double old_lambda = lambda;
-
-        // Find lambda which gives point delta pixels away from x.
-        std::pair<double, double> interval = boost::math::tools::bisect(
-              boost::bind(errorInDistanceFromPoint, x, _1, A, B, delta,
-                other->intrinsics()),
-              lambda_min, lambda,
-              boost::math::tools::eps_tolerance<double>(16));
-        lambda = interval.second;
-
-        // Guard against limit cycles.
-        CHECK(lambda != old_lambda) << "Entered limit cycle";
-
-        // Add lambda to the list.
-        lambdas.push_back(lambda);
-
-        // Update position.
-        cv::Mat X = A + lambda * B;
-        x = imagePointFromHomogeneous(X);
-        x = other->intrinsics().distortAndUncalibrate(x);
-
-        DLOG(INFO) << "x(" << lambda << ") => " << x;
-      }
-    }
-
-    LOG(INFO) << "Quantized ray into " << lambdas.size() << " positions";
-  }
-}
-
 void findMultiviewTrack(const Track<cv::Point2d>& track,
-                        const CameraPose& pose,
-                        const std::vector<OtherView>& other_views,
-                        MultiviewTrack<cv::Point2d>& multiview_track) {
-  int num_other_views = other_views.size();
-  int num_views = num_other_views + 1;
+                        const std::vector<Camera>& cameras,
+                        int selected,
+                        MultiviewTrack<cv::Point2d>& multiview_track,
+                        double lambda1,
+                        double lambda2) {
+  int num_views = cameras.size();
 
   multiview_track = MultiviewTrack<cv::Point2d>(num_views);
 
-  std::vector<Camera> other_cameras;
-  std::transform(other_views.begin(), other_views.end(),
-      std::back_inserter(other_cameras), extractCameraFromOtherView);
+  LOG(INFO) << "Quantizing 3D ray";
 
   // For dynamic program, need to find the extent of the 3D ray in each frame.
-  Track<cv::Point2d>::const_iterator point;
-  for (point = track.begin(); point != track.end(); ++point) {
-    findExtentOfRay(point->second, pose, other_cameras);
+  typedef std::map<double, cv::Point3d> Ray;
+  std::vector<Ray> rays;
+  {
+    Track<cv::Point2d>::const_iterator point;
+    for (point = track.begin(); point != track.end(); ++point) {
+      Ray ray;
+      quantizeRay(point->second, cameras, selected, FLAGS_delta, ray);
+      rays.push_back(Ray());
+      rays.back().swap(ray);
+    }
   }
+
+  LOG(INFO) << "Constructing Viterbi problem";
+
+  // Construct unary costs.
+  std::deque<std::vector<double> > unary_costs;
+  {
+    std::vector<Ray>::const_iterator ray;
+    for (ray = rays.begin(); ray != rays.end(); ++ray) {
+      std::vector<double> costs;
+
+      Ray::const_iterator point;
+      for (point = ray->begin(); point != ray->end(); ++point) {
+        double cost = 0;
+
+        std::vector<Camera>::const_iterator camera;
+        for (camera = cameras.begin(); camera != cameras.end(); ++camera) {
+          // Compute cost.
+          cost += 0;
+        }
+        costs.push_back(cost);
+      }
+
+      unary_costs.push_back(std::vector<double>());
+      unary_costs.back().swap(costs);
+    }
+  }
+
+  // Construct binary costs.
+  std::vector<cv::Mat> binary_costs;
+  if (rays.size() > 1) {
+    std::vector<Ray>::const_iterator ray2 = rays.begin();
+    std::vector<Ray>::const_iterator ray1;
+
+    for (ray1 = ray2++; ray2 != rays.end(); ray1 = ray2++) {
+      int n = ray1->size();
+      int m = ray2->size();
+
+      cv::Mat costs = cv::Mat_<double>(m, n, 0.);
+
+      // Compute cost of moving from each position to another.
+      int index1 = 0;
+      Ray::const_iterator point1;
+      for (point1 = ray1->begin(); point1 != ray1->end(); ++point1) {
+        cv::Point3d x1 = point1->second;
+
+        int index2 = 0;
+        Ray::const_iterator point2;
+        for (point2 = ray2->begin(); point2 != ray2->end(); ++point2) {
+          cv::Point3d x2 = point2->second;
+
+          // Cost of moving from x1 to x2 in 3D.
+          double cost = lambda2 * cv::norm(x1 - x2);
+
+          std::vector<Camera>::const_iterator camera;
+          for (camera = cameras.begin(); camera != cameras.end(); ++camera) {
+            // Compute appearance cost.
+            cost += 0;
+          }
+
+          costs.at<double>(index2, index1) = cost;
+          index2 += 1;
+        }
+        index1 += 1;
+      }
+
+      binary_costs.push_back(costs);
+    }
+  }
+
+  LOG(INFO) << "Solving dynamic program";
+  std::vector<int> solution;
+  solveViterbi(unary_costs, binary_costs, solution);
 }
 
 void findMultiviewTracks(
     const TrackList<cv::Point2d>& tracks,
-    const CameraPose& camera,
-    const std::vector<OtherView>& other_views,
-    MultiviewTrackList<cv::Point2d>& multiview_tracks) {
-  int num_views = other_views.size() + 1;
+    const std::vector<Camera>& cameras,
+    int selected,
+    MultiviewTrackList<cv::Point2d>& multiview_tracks,
+    int lambda1,
+    int lambda2) {
+  int num_views = cameras.size();
   multiview_tracks = MultiviewTrackList<cv::Point2d>(num_views);
 
   TrackList<cv::Point2d>::const_iterator track;
   for (track = tracks.begin(); track != tracks.end(); ++track) {
     // Find match for this track.
     MultiviewTrack<cv::Point2d> multiview_track;
-    findMultiviewTrack(*track, camera, other_views, multiview_track);
+    findMultiviewTrack(*track, cameras, selected, multiview_track, lambda1,
+        lambda2);
 
     // Swap into end of list.
     multiview_tracks.push_back(MultiviewTrack<cv::Point2d>());
@@ -331,8 +262,7 @@ int main(int argc, char** argv) {
   CHECK(main_view < num_views);
 
   // Load properties of each view.
-  std::vector<OtherView> other_views;
-  Camera camera;
+  std::vector<Camera> cameras;
 
   CameraPoseReader extrinsics_reader;
   CameraPropertiesReader intrinsics_reader;
@@ -351,29 +281,12 @@ int main(int argc, char** argv) {
     ok = load(extrinsics_file, extrinsics, extrinsics_reader);
     CHECK(ok) << "Could not load extrinsics";
 
-    Camera view_camera(intrinsics, extrinsics);
-
-    if (view == main_view) {
-      // Set intrinsics of main view.
-      camera = view_camera;
-    } else {
-      OtherView other_view;
-      other_view.index = view;
-      other_view.camera = view_camera;
-      other_views.push_back(other_view);
-    }
+    cameras.push_back(Camera(intrinsics, extrinsics));
   }
-
-  // Undistort points in original view.
-  TrackList<cv::Point2d> undistorted_tracks;
-  std::transform(input_tracks.begin(), input_tracks.end(),
-      std::back_inserter(undistorted_tracks),
-      boost::bind(calibrateAndUndistortTrack, _1, camera.intrinsics()));
 
   // Find multiview tracks.
   MultiviewTrackList<cv::Point2d> multiview_tracks;
-  findMultiviewTracks(undistorted_tracks, camera.extrinsics(), other_views,
-      multiview_tracks);
+  findMultiviewTracks(input_tracks, cameras, main_view, multiview_tracks, 1, 1);
 
   // Save points and tracks out.
   ImagePointWriter<double> point_writer;
